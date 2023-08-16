c514c98c6f63bfadb4bbc708076f610512caa8f2
SakuraaGT <huddleonpc@gmail.com>
2023-08-16 15:58:18 -0700

    Release

---
+++ Sakuraa-SkyMapFix/Plugin.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+
+namespace Sakuraa_SkyMapFix
+{
+	[ModdedGamemode]
+	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+        private GameObject skyJungleBottom;
+        private GameObject cityToSkyJungle;
+        private GameObject city;
+
+        void Start()
+        {
+            skyJungleBottom = GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Sky Jungle Bottom (1)");
+            cityToSkyJungle = GameObject.Find("Environment Objects/LocalObjects_Prefab/CityToSkyJungle");
+            city = GameObject.Find("Environment Objects/LocalObjects_Prefab/City");
+
+            if (city == null)
+            {
+                Debug.LogError("DNM: City GameObject not found!");
+                return;
+            }
+
+            if (skyJungleBottom != null)
+            {
+                skyJungleBottom.SetActive(false);
+            }
+
+            if (cityToSkyJungle != null)
+            {
+                cityToSkyJungle.SetActive(false);
+            }
+        }
+
+        void Update()
+        {
+            if (city == null)
+            {
+                Debug.LogError("DNM: City GameObject not found!");
+                return;
+            }
+
+            if (city.activeSelf)
+            {
+                if (skyJungleBottom != null)
+                {
+                    skyJungleBottom.SetActive(true);
+                }
+
+                if (cityToSkyJungle != null)
+                {
+                    cityToSkyJungle.SetActive(true);
+                }
+            }
+            else
+            {
+                if (skyJungleBottom != null)
+                {
+                    skyJungleBottom.SetActive(false);
+                }
+
+                if (cityToSkyJungle != null)
+                {
+                    cityToSkyJungle.SetActive(false);
+                }
+            }
+        }
+    }
+}
---
+++ Sakuraa-SkyMapFix/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Sakuraa_SkyMapFix
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.sakuraa.gorillatag.sakuraa-skymapfix";
+		public const string Name = "Sakuraa_SkyMapFix";
+		public const string Version = "1.0.0";
+	}
+}
